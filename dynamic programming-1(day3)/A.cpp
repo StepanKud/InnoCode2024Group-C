6f3d72e9bf628aea14c7d8c769d454807cd99ad7
mont1cc <141335185+StepanKud@users.noreply.github.com>
2024-07-23 23:04:41 +0400

    Create A.cpp

---
+++ dynamic programming-1(day3)/A.cpp
@@ -0,0 +1,42 @@
+#include<algorithm>
+#include<iostream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<deque>
+#include<cmath>
+#include<array>
+#include<queue>
+#include<set>
+#include<map>
+
+using namespace std;
+
+typedef unsigned long long ull;//From 0 to 18 446 744 073 709 551 615. byte:8
+typedef long double ld;//In the range +/- 3.4E-4932 to 1.1E+4932 byte:10-16
+typedef long long ll;//From −9 223 372 036 854 775 808 to 9 223 372 036 854 775 807. byte:8
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+typedef vector<ll> vl;
+
+ll main() {
+	ios::sync_with_stdio(0);//passively speeds up code
+	cin.tie(0);//passively speeds up code
+	cout.tie(0);//passively speeds up code
+	ll n, k;
+	cin >> n >> k;
+	vl dp(n);//typedef vector<ll> vl 
+	dp[0] = 1;//the basis
+	dp[1] = 1;//the basis
+	for (ll i = 2; i < n; i++) {
+		for (ll j = 1; j <= k; j++) {//jump from 1 to k
+			if (j <= i) {
+				dp[i] += dp[i - j];
+			}
+			else {
+				j = k;
+			}
+		}
+	}
+	cout << dp[n - 1];
+}
